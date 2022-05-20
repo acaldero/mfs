d8964e3a3a1c43af0549c25bee5a828de551365f
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2022-05-20 19:12:44 +0200

    minor updates

--- staging/stk_mid_log.h
+++ staging/stk_mid_log.h
@@ -16,15 +16,16 @@
       #include "include/stk_fs.h"
 
 
+   /* ... Const / Constantes ............................................ */
+
+      #define FS_TAG_NULL        "stk_mid_log"
+
+
    /* ... Functions / Funciones ......................................... */
 
-      // register + unregister
-      int     stk_mid_log_register   ( stk_fs_t *fsi ) ;
-      int     stk_mid_log_unregister ( stk_fs_t *fsi ) ;
-
-      // init + finalize
-      int     stk_mid_log_init     ( void ) ;
-      int     stk_mid_log_finalize ( void ) ;
+      // Init + finalize
+      int     stk_mid_null_init     ( stk_fs_t *fsi, stk_fs_t *low_fsi ) ;
+      int     stk_mid_null_finalize ( stk_fs_t *fsi ) ;
 
       // File API
       int     stk_mid_log_creat  ( char *path, mode_t mode ) ;
